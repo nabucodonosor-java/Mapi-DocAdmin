49de157de66d92c3b37a2252d92f3e5a8d8c004a
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-22 16:28:17 -0300

    Fix BASE_URL

--- frontend/src/core/utils/request.ts
+++ frontend/src/core/utils/request.ts
@@ -7,9 +7,7 @@ type LoginData = {
     password: string;
 }
 
-// const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-
-const BASE_URL = 'https://doc-admin.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 axios.interceptors.response.use(function(response) {
     return response;
