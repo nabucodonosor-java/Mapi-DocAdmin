f682fab5f57904df0456e0f9af5bd09947122973
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-22 16:06:57 -0300

    Add react-paginate

--- frontend/src/core/components/Pagination/index.tsx
+++ frontend/src/core/components/Pagination/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
-
+import ReactPaginate from 'react-paginate';
 import './styles.scss';
-import { generateList } from '../../utils/list';
-
 
 type Props = {
     totalPages: number;
@@ -12,29 +10,22 @@ type Props = {
 }
 
 const Pagination = ({ totalPages, activePage, onChange }: Props) => {
-    const items = generateList(totalPages);
-    const previousClass = totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
-    const nextClass = (activePage + 1) < totalPages ? 'page-active' : 'page-inactive';
+    const renderIcon = () => (
+        <ArrowIcon
+            className={`pagination-previous`}
+            data-testid="arrow-icon-previous"
+        />
+    );
 
     return (
         <div className="pagination-container">
-            <ArrowIcon 
-            className={`pagination-previous ${previousClass}`}
-            onClick={() => onChange(activePage - 1)}
-            />
-            {items.map(item => (
-            <div 
-                key={item}
-                className={`pagination-item ${item === activePage ? 'active' : ''}`}
-                onClick={() => onChange(item)}
-            >
-                {item + 1}
-            </div>
-            ))}    
-            <ArrowIcon 
-                className={`pagination-next ${nextClass}`}
-                onClick={() => onChange(activePage  + 1)}
-            />
+           <ReactPaginate 
+            pageCount={totalPages}
+            pageRangeDisplayed={5}
+            marginPagesDisplayed={1}
+            onPageChange={selectedItem => onChange(selectedItem.selected)}
+           /> 
+           
         </div>
     );
 }
